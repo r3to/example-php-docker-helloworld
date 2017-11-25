589e3492b6c1b2319a5dcbc8bd6bf5dc01cf7957
Reto Lehnherr <reto.lehnherr@mac.com>
2017-11-25 21:12:46 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -53,7 +53,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Hallo <?php echo 'Velo'?></h1>
+        <h1>Hallo <?php echo 'V3lo'?></h1>
         <p class="lead">APPUiO Example Dockerfile PHP</p>
       </div>
 
